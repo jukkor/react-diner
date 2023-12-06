aed914955843fbe2f6e3a32de5abde463499c296
Jukka Korhonen <jukka.korhonen@outlook.com>
2023-12-06 12:01:14 +0200

    app and index general styles, react-router-dom

--- react-dine-frontend/src/App.jsx
+++ react-dine-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Link } from "react-router-dom";
-import Home from './Home';
-import ProductList from './ProductList';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 import './App.css';
 
@@ -9,19 +9,23 @@ function App() {
 
   return (
     <>
-      <nav>
-        <ul>
-          <li><Link to="/" >Home</Link></li>
-          <li><Link to="/productlist" >Product List</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productlist" element={<ProductList />} />
-      </Routes>
-      <footer className="footer">
-        <p>&copy; 2023 Big Kebab. All rights reserved.</p>
-      </footer>
+      <div className="appContainer">
+        <nav>
+          <ul>
+            <li><Link to="/" >Home</Link></li>
+            <li><Link to="/productlist" >Product List</Link></li>
+          </ul>
+        </nav>
+        <div className="contentWindow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/productlist" element={<ProductList />} />
+          </Routes>
+        </div>
+        <footer className="footer">
+          <p>&copy; 2023 Big Kebab. All rights reserved.</p>
+        </footer>
+      </div>
     </>
   )
 }
