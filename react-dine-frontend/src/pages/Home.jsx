17bf9b89ef1647bb77091f330099a8a895c8a2d8
Jukka Korhonen <jukka.korhonen@outlook.com>
2023-12-06 12:02:13 +0200

    Placeholder homepage

---
+++ react-dine-frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css'
+
+const Home = () => {
+
+  return (
+    <>
+      <div className='home'>
+        <h1>HOME</h1>
+        <p>Lorem ipsum</p>
+      </div>
+    </>
+  )
+}
+
+export default Home;
