7250bd415d6ff2c2582b5201466ffac910628df9
Jukka Korhonen <jukka.korhonen@outlook.com>
2023-12-06 10:10:25 +0200

    react-router and setting up app.jsx

--- react-dine-frontend/src/App.jsx
+++ react-dine-frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, Link } from "react-router-dom";
+import Home from './Home';
+import ProductList from './ProductList';
+
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <ul>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/productlist" >Product List</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productlist" element={<ProductList />} />
+      </Routes>
+      <footer className="footer">
+        <p>&copy; 2023 Big Kebab. All rights reserved.</p>
+      </footer>
     </>
   )
 }
 
-export default App
+export default App;
